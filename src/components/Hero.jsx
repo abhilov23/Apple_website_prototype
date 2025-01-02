cf1f7d9c1567bb208f4a929df5c46ee8acfcff5c
Abhilov23 <abhilovgupta01@gmail.com>
2025-01-03 01:16:54 +0530

    finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,49 @@
-import gsap from "gsap";
-import {useGSAP} from "@gsap/react";
-import { heroVideo, smallHeroVideo } from "../utils";
-import { useState, useEffect } from "react";
-
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
+import { useEffect, useState } from 'react';
 
 const Hero = () => {
-  const [videoSrc, setVideoSrc] =  useState(window.innerWidth < 760 ?smallHeroVideo : heroVideo)
+  const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+
   const handleVideoSrcSet = () => {
-    if(window.innerWidth < 760){
-       setVideoSrc(smallHeroVideo);
-    }
-    else{
-       setVideoSrc(heroVideo);
+    if(window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo)
+    } else {
+      setVideoSrc(heroVideo)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     window.addEventListener('resize', handleVideoSrcSet);
-    return () => window.removeEventListener('resize', handleVideoSrcSet);  // Clean up the event listener when the component unmounts.
-  },[])
 
+    return () => {
+      window.removeEventListener('reisze', handleVideoSrcSet)
+    }
+  }, [])
 
-  useGSAP(()=>{
-   gsap.to('#hero', {
-    opacity:1,
-    delay:1.5
-   }),
-   gsap.to('#cta',{ opacity:1,y:-50, delay:2})
-  },[])
-
+  useGSAP(() => {
+    gsap.to('#hero', { opacity: 1, delay: 2 })
+    gsap.to('#cta', { opacity: 1, y: -50, delay: 2 })
+  }, [])
 
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-      <p id="hero" className="hero-title">iPhone 15 Pro</p>
+        <p id="hero" className="hero-title">iPhone 15 Pro</p>
         <div className="md:w-10/12 w-9/12">
-         <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
-          <source src={videoSrc} type="video/mp4" />
-         </video>
+          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+            <source src={videoSrc} type="video/mp4" />
+          </video>
         </div>
       </div>
-      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
-       <a href="#highlights" className="btn">Buy</a>
-       <p className="font-normal text-xl">From $199/month or $1999</p>
+
+      <div
+        id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+      >
+        <a href="#highlights" className="btn">Buy</a>
+        <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
     </section>
   )
